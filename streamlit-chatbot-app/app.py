8ca07fa33b7e56c8cf0abc66b024ea13998ad55e
Sid Murching <sid.murching@databricks.com>
2025-04-05 00:06:15 -0700

    WIP - going to support a list of response messages from the LLM in the message history
    Signed-off-by: Sid Murching <sid.murching@databricks.com>

--- streamlit-chatbot-app/app.py
+++ streamlit-chatbot-app/app.py
@@ -1,12 +1,16 @@
 import logging
 import os
 import streamlit as st
-from model_serving_utils import query_endpoint
+from model_serving_utils import query_endpoint, endpoint_supports_feedback, submit_feedback
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-assert os.getenv('SERVING_ENDPOINT'), "SERVING_ENDPOINT must be set in app.yaml."
+SERVING_ENDPOINT = os.getenv('SERVING_ENDPOINT')
+assert SERVING_ENDPOINT is not None, "SERVING_ENDPOINT must be set in app.yaml."
+
+ENDPOINT_SUPPORTS_FEEDBACK = endpoint_supports_feedback(SERVING_ENDPOINT)
+
 
 def get_user_info():
     headers = st.context.headers
@@ -22,9 +26,6 @@ user_info = get_user_info()
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-if "feedback" not in st.session_state:
-    st.session_state.feedback = {}
-
 if "visibility" not in st.session_state:
     st.session_state.visibility = "visible"
     st.session_state.disabled = False
@@ -33,6 +34,19 @@ if "visibility" not in st.session_state:
 st.title("🧱 Chatbot App")
 st.write("A basic chatbot using your own serving endpoint.")
 
+def render_assistant_message_feedback(i, request_id):
+    def save_feedback(index):
+        # st.session_state.messages[index]["feedback"] = st.session_state[f"feedback_{index}"]
+        submit_feedback(
+            endpoint=SERVING_ENDPOINT,
+            request_id=request_id,
+            rating=st.session_state[f"feedback_{index}"]
+        )
+    selection = st.feedback("thumbs", key=f"feedback_{i}", on_change=save_feedback, args=[i])
+    if selection is not None:
+        st.markdown(f"Feedback received: {'👍' if selection == 1 else '👎'}")
+
+
 # --- Render chat history ---
 for i, message in enumerate(st.session_state.messages):
     with st.chat_message(message["role"]):
@@ -40,19 +54,35 @@ for i, message in enumerate(st.session_state.messages):
 
         def save_feedback(index):
             print(f"@SID got feedback {st.session_state[f'feedback_{index}']} for message {index}")
-            st.session_state.messages[index]["feedback"] = st.session_state[f"feedback_{index}"]
+            # st.session_state.messages[index]["feedback"] = st.session_state[f"feedback_{index}"]
 
+        # TODO: should we drop this? or also call render_assistant_message_feedback,
+        # but that results in duplicate key errors
         if message["role"] == "assistant":
             selection = st.feedback("thumbs", key=f"feedback_{i}", on_change=save_feedback, args=[i])
             if selection is not None:
                 st.markdown(f"Feedback received: {'👍' if selection == 1 else '👎'}")
+            print("@SID rendering feedback for existing assistant message at index ", i)
+            # render_assistant_message_feedback(i)
 
 # --- Chat input (must run BEFORE rendering messages) ---
 if prompt := st.chat_input("What is up?"):
+    # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
-    response = query_endpoint(
-        endpoint_name=os.getenv("SERVING_ENDPOINT"),
-        messages=st.session_state.messages,
-        max_tokens=400,
-    )["content"]
-    st.session_state.messages.append({"role": "assistant", "content": response})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        # Query the Databricks serving endpoint
+        response_obj, request_id_opt = query_endpoint(
+            endpoint_name=SERVING_ENDPOINT,
+            messages=st.session_state.messages,
+            max_tokens=400,
+            return_traces=ENDPOINT_SUPPORTS_FEEDBACK
+        )
+        response = response_obj["content"]
+        st.markdown(response)
+        if request_id_opt is not None:
+            render_assistant_message_feedback(len(st.session_state.messages), request_id_opt)
+    st.session_state.messages.append({"role": "assistant", "content": response, "request_id": request_id_opt})
--- streamlit-chatbot-app/model_serving_utils.py
+++ streamlit-chatbot-app/model_serving_utils.py
@@ -1,19 +1,69 @@
 from mlflow.deployments import get_deploy_client
+from databricks.sdk import WorkspaceClient
+import json
 
-def _query_endpoint(endpoint_name: str, messages: list[dict[str, str]], max_tokens) -> list[dict[str, str]]:
+def _query_endpoint(endpoint_name: str, messages: list[dict[str, str]], max_tokens, return_traces) -> list[dict[str, str]]:
     """Calls a model serving endpoint."""
+    inputs={'messages': messages, "max_tokens": max_tokens},
+    if return_traces:
+        inputs['databricks_options'] = {'return_trace': True}
     res = get_deploy_client('databricks').predict(
         endpoint=endpoint_name,
         inputs={'messages': messages, "max_tokens": max_tokens},
     )
+    request_id = res.get("databricks_output", {}).get("databricks_request_id")
     if "messages" in res:
-        return res["messages"]
+        return res["messages"], request_id
     elif "choices" in res:
-        return [res["choices"][0]["message"]]
+        return [res["choices"][0]["message"]], request_id
     raise Exception("This app can only run against:"
                     "1) Databricks foundation model or external model endpoints with the chat task type (described in https://docs.databricks.com/aws/en/machine-learning/model-serving/score-foundation-models#chat-completion-model-query)"
-                    "2) Databricks agent serving endpoints that implmeent the conversational agent schema documented "
+                    "2) Databricks agent serving endpoints that implement the conversational agent schema documented "
                     "in https://docs.databricks.com/aws/en/generative-ai/agent-framework/author-agent")
 
-def query_endpoint(endpoint_name, messages, max_tokens):
-    return _query_endpoint(endpoint_name, messages, max_tokens)[-1]
+def query_endpoint(endpoint_name, messages, max_tokens, return_traces):
+    """
+    Query an endpoint, returning the string message content and request
+    ID for feedback
+    """
+    response_messages, request_id = _query_endpoint(endpoint_name, messages, max_tokens, return_traces)
+    # TODO: render intermediate tool calls as well
+    return response_messages[-1], request_id
+
+
+def submit_feedback(endpoint, request_id, rating):
+    """Submit feedback to the agent."""
+    text_assessments = [] if rating is None else [{
+        "ratings": {
+            "answer_correct": {"value": rating},
+        },
+        "free_text_comment": None
+    }]
+
+    proxy_payload = {
+        "dataframe_records": [
+            {
+                "source": json.dumps({
+                    "id": "user@company.com",  # Or extract from auth
+                    "type": "human"
+                }),
+                "request_id": request_id,
+                "text_assessments": json.dumps(text_assessments),
+                "retrieval_assessments": json.dumps([]),
+            }
+        ]
+    }
+
+    w = WorkspaceClient()
+    return w.api_client.do(
+        method='POST',
+        path=f"/serving-endpoints/{endpoint}/served-models/feedback/invocations",
+        body=proxy_payload,
+    )
+
+
+def endpoint_supports_feedback(endpoint_name):
+    w = WorkspaceClient()
+    endpoint = w.serving_endpoints.get(endpoint_name)
+    return "feedback" in [entity.entity_name for entity in endpoint.config.served_entities]
+

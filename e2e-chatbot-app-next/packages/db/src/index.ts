03b18d7d2a0590b8f2f48f9e72fbcb6298d74d04
Emil Lysgaard <emil.lysgaard@databricks.com>
2025-10-27 08:24:16 +0100

    fix migrate misfire

--- e2e-chatbot-app-next/packages/db/src/index.ts
+++ e2e-chatbot-app-next/packages/db/src/index.ts
@@ -1,6 +1,5 @@
 export * from './schema';
 export * from './queries';
-export * from './migrate';
 export * from './connection';
 export * from './connection-core';
 export * from './connection-pool';

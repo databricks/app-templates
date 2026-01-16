b49e59aa3b8fc2d972e633b02ec72b910ca4a06d
Jenny <jenny.sun@databricks.com>
2026-01-16 14:26:00 -0800

    copy agent-langgraph into agent-langgraph-long-term-memory

---
+++ agent-langgraph-long-term-memory/agent_server/agent.py
@@ -0,0 +1,64 @@
+from typing import AsyncGenerator, Optional
+
+import mlflow
+from databricks.sdk import WorkspaceClient
+from databricks_langchain import ChatDatabricks, DatabricksMCPServer, DatabricksMultiServerMCPClient
+from langchain.agents import create_agent
+from mlflow.genai.agent_server import invoke, stream
+from mlflow.types.responses import (
+    ResponsesAgentRequest,
+    ResponsesAgentResponse,
+    ResponsesAgentStreamEvent,
+    to_chat_completions_input,
+)
+
+from agent_server.utils import (
+    get_databricks_host_from_env,
+    get_user_workspace_client,
+    process_agent_astream_events,
+)
+
+mlflow.langchain.autolog()
+sp_workspace_client = WorkspaceClient()
+
+
+def init_mcp_client(workspace_client: WorkspaceClient) -> DatabricksMultiServerMCPClient:
+    host_name = get_databricks_host_from_env()
+    return DatabricksMultiServerMCPClient(
+        [
+            DatabricksMCPServer(
+                name="system-ai",
+                url=f"{host_name}/api/2.0/mcp/functions/system/ai",
+            ),
+        ]
+    )
+
+
+async def init_agent(workspace_client: Optional[WorkspaceClient] = None):
+    mcp_client = init_mcp_client(workspace_client or sp_workspace_client)
+    tools = await mcp_client.get_tools()
+    return create_agent(tools=tools, model=ChatDatabricks(endpoint="databricks-gpt-5-2"))
+
+
+@invoke()
+async def non_streaming(request: ResponsesAgentRequest) -> ResponsesAgentResponse:
+    outputs = [
+        event.item
+        async for event in streaming(request)
+        if event.type == "response.output_item.done"
+    ]
+    return ResponsesAgentResponse(output=outputs)
+
+
+@stream()
+async def streaming(
+    request: ResponsesAgentRequest,
+) -> AsyncGenerator[ResponsesAgentStreamEvent, None]:
+    # user_workspace_client = get_user_workspace_client()
+    agent = await init_agent()
+    messages = {"messages": to_chat_completions_input([i.model_dump() for i in request.input])}
+
+    async for event in process_agent_astream_events(
+        agent.astream(input=messages, stream_mode=["updates", "messages"])
+    ):
+        yield event
